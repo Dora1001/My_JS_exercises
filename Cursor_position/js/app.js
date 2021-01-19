3535b7bda4d080a78bdf3d7164a45a2fda94c2b0
Dorota Janus <dorota@janus-grafik.pl>
2021-01-19 13:40:25 +0100

    cursor position

---
+++ Cursor_position/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.querySelector("div");
+    const span = document.querySelectorAll("span");
+    const globalX = document.getElementById("globalX");
+    const globalY = document.getElementById("globalY");
+    const localX = document.getElementById("localX");
+    const localY = document.getElementById("localY");
+    div.addEventListener("mousemove", e => {
+        globalX.innerHTML = e.clientX;
+        globalY.innerHTML = e.clientY;
+        localX.innerHTML = e.offsetX;
+        localY.innerHTML = e.offsetY;
+    })
+})
