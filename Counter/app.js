78d768b949b666bd6a36307f62e9948060c7bcdd
Dorota Janus <dorotaja101@gmail.com>
2021-02-14 20:03:19 +0100

    Color counter

--- Counter/app.js
+++ Counter/app.js
@@ -6,19 +6,27 @@ const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
     btn.addEventListener('click', function (e) {
-        const style = e.currentTarget.classList;
-        if (style.contains('decrease')) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
             count--;
-
         }
-        else if (style.contains('increase')) {
-
+        else if (styles.contains('increase')) {
             count++;
         }
-
         else {
             count = 0;
         }
+        if (count < 0){
+            value.style.color = "red"
+        }
+        if (count > 0){
+            value.style.color = "green"
+        }
+        if  (count === 0)
+        {
+            value.style.color = "black"
+        }
+
         value.textContent = count;
 
     });
